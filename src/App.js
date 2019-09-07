a77c06eaa8143253e03489887cb192302f8f6abd
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-09-06 21:43:15 -0400

    Added react router and added 3 routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="main-container">
-        <h1>Groc-List</h1>
-      </div>
+      <Router>
+
+        <Route path="/" exact component={ItemList} />
+        <Route path="/edit/:id" component={EditItem} />
+        <Route path="/create" component={CreateItem} />
+        
+        <div className="main-container">
+          <h1>Groc-List</h1>
+        </div>
+      </Router>
     )
   }
 }
