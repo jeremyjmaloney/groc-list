3153446fe82e95d675f4adefab673d4af2094e9c
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-09-06 23:47:16 -0400

    Added form for creating new item in create item component

--- src/components/create-item.component.js
+++ src/components/create-item.component.js
@@ -5,42 +5,50 @@ export default class CreateItem extends Component {
     super(props)
     this.state = {
       item_description: '',
-      item_responsible: '',
-      item_priority: '',
+      item_priority: 'Low',
       item_completed: false
     }
+    // this.onChangeItemDescription = this.onChangeItemDescription.bind(this);
+    // this.onChangeItemPriority = this.onChangeItemPriority.bind(this);
+    // this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onChangeItemDescription(event) => {
+  onChangeItemDescription = (event) => {
     this.setState({
       item_description: event.target.value
     });
   };
 
-  onChangeItemResponsible(event) => {
+  onChangeItemPriority = (event) => {
     this.setState({
-      todo_responsible: event.target.value
+      item_priority: event.target.value
     });
   };
 
-  onChangeItemPriority(event) => {
-    this.setState({
-      item_priority: event.targer.value
-    });
-  };
-
-  onSubmit(event) => {
+  onSubmit = (event) => {
     event.preventDefaultl();
     console.log('submitted');
     console.log(`item-desc: ${this.state.item_description}`);
-    console.log(`item-resp: ${this.state.item_responsible}`);
     console.log(`item-prio: ${this.state.item_priority}`);
   };
 
   render() {
     return (
-      <div>
-        <h1>This is the Create Item Component</h1>
+      <div className='create-form'>
+        <h2>ADD NEW ITEM</h2>
+        <form onSubmit={this.onSubmit}>
+          <label>ITEM: </label>
+          <input type='text' value={this.state.item_description} onChange={this.onChangeItemDescription} />
+
+          <label>PRIORITY</label>
+          <input type='radio' name='priorityOptions' id='priorityLow' value='Low' checked={this.state.item_priority==='Low'} onChange={this.onChangeItemPriority} />
+          <label>LOW</label>
+
+          <input type='radio' name='priorityOptions' id='priorityHigh' value='High' checked={this.state.item_priority==='High'} onChange={this.onChangeItemPriority} />
+          <label>HIGH</label>
+
+          <input className='submit-btn' type='submit' value='ADD ITEM' />
+        </form>
       </div>
     )
   }
