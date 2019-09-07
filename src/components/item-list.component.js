902681604f6c5ba1daffe7d72b3103405fc4ff27
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-09-07 03:30:25 -0400

    Able to print out all items in the list with a link to the edit page for each item

--- src/components/item-list.component.js
+++ src/components/item-list.component.js
@@ -1,10 +1,36 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 export default class ItemList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:4000/items')
+      .then(response => {
+        this.setState({
+          items: response.data
+        })
+      })
+  }
+
   render() {
     return (
       <div>
-        <h1>This is the Item List component</h1>
+        <h1>ITEMS LIST</h1>
+        {this.state.items.map((item, index) => {
+          return (
+            <div className='item' key={index}>
+              <h4>{item.item_description}</h4>
+              <Link to={`/edit/${item._id}`}>Edit</Link>
+            </div>
+          )
+        })}
       </div>
     )
   }
