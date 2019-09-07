e29c74503c35c05f3185bed14e42eb55c17f641f
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-09-06 22:26:09 -0400

    Added navbar to App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,27 @@ import CreateItem from './components/create-item.component';
 import EditItem from './components/edit-item.component';
 import ItemList from './components/item-list.component';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="main-container">
-          <h1>Groc-List</h1>
+        <div className='main-container'>
+          <nav className='navbar'>
+            <a className='navbar-brand'>
+              <img src='./checkmark.jpg' alt='checkmark'/>
+            </a>
+            <Link to='/' className='navbar-logo'>Groc-List App</Link>
+            <ul className='navbar-elements'>
+              <li className='navbar-item'>
+                <Link to='/' className='nav-link'>ITEMS</Link>
+              </li>
+              <li className='navbar-item'>
+                <Link to='/create' className='nav-link'>ADD ITEM</Link>
+              </li>
+            </ul>
+          </nav>
 
           <Route path="/" exact component={ItemList} />
           <Route path="/edit/:id" component={EditItem} />
