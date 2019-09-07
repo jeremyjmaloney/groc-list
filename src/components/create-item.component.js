64f517cb5e6a1a635c69856e38850912017b9de6
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-09-06 23:13:29 -0400

    Added state and functions to handle events in create item component

--- src/components/create-item.component.js
+++ src/components/create-item.component.js
@@ -1,6 +1,42 @@
 import React, {Component} from 'react';
 
 export default class CreateItem extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      item_description: '',
+      item_responsible: '',
+      item_priority: '',
+      item_completed: false
+    }
+  }
+
+  onChangeItemDescription(event) => {
+    this.setState({
+      item_description: event.target.value
+    });
+  };
+
+  onChangeItemResponsible(event) => {
+    this.setState({
+      todo_responsible: event.target.value
+    });
+  };
+
+  onChangeItemPriority(event) => {
+    this.setState({
+      item_priority: event.targer.value
+    });
+  };
+
+  onSubmit(event) => {
+    event.preventDefaultl();
+    console.log('submitted');
+    console.log(`item-desc: ${this.state.item_description}`);
+    console.log(`item-resp: ${this.state.item_responsible}`);
+    console.log(`item-prio: ${this.state.item_priority}`);
+  };
+
   render() {
     return (
       <div>
