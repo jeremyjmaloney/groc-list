cebd63069eefbded53dfa9ba6ba33b3250217523
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-09-06 21:57:09 -0400

    Imported components to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React, {Component} from "react";
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+import CreateItem from './components/create-item.component';
+import EditItem from './components/edit-item.component';
+import ItemList from './components/item-list.component';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,13 +12,12 @@ class App extends Component {
   render() {
     return (
       <Router>
-
-        <Route path="/" exact component={ItemList} />
-        <Route path="/edit/:id" component={EditItem} />
-        <Route path="/create" component={CreateItem} />
-        
         <div className="main-container">
           <h1>Groc-List</h1>
+
+          <Route path="/" exact component={ItemList} />
+          <Route path="/edit/:id" component={EditItem} />
+          <Route path="/create" component={CreateItem} />
         </div>
       </Router>
     )
--- src/components/create-item.component.js
+++ src/components/create-item.component.js
@@ -4,7 +4,7 @@ export default class CreateItem extends Component {
   render() {
     return (
       <div>
-        <h1>This is the Create Item Component<?h1>
+        <h1>This is the Create Item Component</h1>
       </div>
     )
   }
